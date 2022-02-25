9226f2f8a66ff8fd15781d123a3099366870d3db
seraphinatarrant <serif@uw.edu>
2022-02-25 13:27:20 +0000

    add option to do only at convergence

--- evaluation/distribution.py
+++ evaluation/distribution.py
@@ -2,7 +2,6 @@ import argparse
 import os
 
 import numpy as np
-import yaml
 
 import pandas as pd
 from scipy.stats import wasserstein_distance
@@ -10,12 +9,16 @@ import sys
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from utils.model_utils import lang2convergence, compressed2convergence, multi_convergence, compressed_multi_convergence
+
 def setup_argparse():
     p = argparse.ArgumentParser()
     p.add_argument('-l', dest='lang', default='ja',
                    help='a the lang to do the analysis on')
     p.add_argument('-o', dest='output', default='analysis/label_distributions/')
     p.add_argument('-r', dest='results_dir', default="results/{}/full_output/")
+    p.add_argument('--convergence', action='store_true', help="only graph models at convergence")
+    p.add_argument('--compressed', action='store_true', help='only used if convergence is true')
     return p.parse_args()
 
 if __name__ == "__main__":
@@ -31,8 +34,20 @@ if __name__ == "__main__":
     }
 
     dists = []
+    if args.convergence:
+        if args.compressed:
+            convergence_dict = compressed2convergence
+            multi_convergence = compressed_multi_convergence
+        else:
+            convergence_dict = lang2convergence
+
     for key in patterns.keys():
         df = pd.read_csv(os.path.join(prefix, patterns[key]))
+        if key == "multi":
+            mask = df["steps"] == multi_convergence
+        else:
+            mask = df["steps"] == convergence_dict[args.lang]
+        df = df[mask]
         vals = np.concatenate([df["label_1"].to_numpy(), df["label_2"].to_numpy()])
         print(len(vals))
         myplot = sns.histplot(data=vals, discrete=True)
