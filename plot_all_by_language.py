20db7005bb6a5bc16940c499518fdb7a6a59e58f
Seraphina Tarrant <s.tarrant@ed.ac.uk>
2022-04-14 17:35:32 +0100

    separated by bias type splitplots

--- plot_all_by_language.py
+++ plot_all_by_language.py
@@ -9,13 +9,7 @@ import matplotlib.pyplot as plt
 from utils.model_utils import lang2convergence, compressed2convergence, multi_convergence, \
     compressed_multi_convergence, balanced_multi_convergence, compressed_balanced_multi_convergence
 
-def setup_argparse():
-    p = argparse.ArgumentParser()
-    p.add_argument('-l', '--lang', dest='lang')
-    p.add_argument('-o', dest='output_dir', default='analysis/plot_all/', help='output dir')
-    
-    return p.parse_args()
-
+from evaluation.create_eval_set import lang2bias
 
 def get_convergence_by_type(model_type, lang):
     """
@@ -27,13 +21,13 @@ def get_convergence_by_type(model_type, lang):
         steps = lang2convergence[lang]
     elif model_type == "multi_xl":
         steps = multi_convergence
-    elif model_type == "mono_compressed":
+    elif model_type == "mono_c":
         steps = compressed2convergence[lang]
-    elif model_type == "multi_xl_compressed":
+    elif model_type == "multi_xl_c":
         steps = compressed_multi_convergence
-    elif model_type == "multi_xl_balanced":
+    elif model_type == "multi_xl_b":
         steps = balanced_multi_convergence
-    elif model_type == "multi_xl_compressed_balanced":
+    elif model_type == "multi_xl_c_b":
         steps = compressed_balanced_multi_convergence
     else:
         sys.exit("Not a valid model type to check for convergence: {}".format(model_type))
@@ -41,30 +35,51 @@ def get_convergence_by_type(model_type, lang):
     return steps
 
 
+def setup_argparse():
+    p = argparse.ArgumentParser()
+    p.add_argument('-l', '--lang', dest='lang')
+    p.add_argument('-o', dest='output_dir', default='analysis/plot_all/', help='output dir')
+    p.add_argument('-pt', '--plot_type', choices=['swarm', 'scatter'], default="swarm")
+
+    return p.parse_args()
+
 
 if __name__ == "__main__":
     args = setup_argparse()
 
-    # TODO maybe see if can switch to boxplot or optionally also include it
-    type_order = ["baseline", "mono", "mono_compressed", "multi_xl", "multi_xl_compressed", "multi_xl_balanced", "multi_xl_compressed_balanced"]
+    print(args)
+
+    type_order = ["baseline", "mono", "mono_c", "multi_xl",
+                  "multi_xl_c", "multi_xl_b", "multi_xl_c_b"]
 
     type2filepattern = {
         "baseline": "results/baseline/{}/{}_ensemble.csv",
         "mono": "results/{}/{}_ensemble.csv",
         "multi_xl": "results/{}/multi+en_{}_ensemble.csv",
-        "mono_compressed": "results/compressed/{}/{}_ensemble.csv",
-        "multi_xl_compressed": "results/compressed/{}/multi+en_{}_ensemble.csv",
-        "multi_xl_balanced": "results/balanced/{}/multi+en_{}_ensemble.csv",
-        "multi_xl_compressed_balanced": "results/balanced/compressed/{}/multi+en_{}_ensemble.csv",
+        "mono_c": "results/compressed/{}/{}_ensemble.csv",
+        "multi_xl_c": "results/compressed/{}/multi+en_{}_ensemble.csv",
+        "multi_xl_b": "results/balanced/{}/multi+en_{}_ensemble.csv",
+        "multi_xl_c_b": "results/balanced/compressed/{}/multi+en_{}_ensemble.csv",
     }
+
+    insert = "full_output"
+    file_insert = "_all_data"
+    if args.plot_type == "swarm":
+        for key, val in type2filepattern.items():
+            _path, _filename = os.path.split(val)
+            _file, _ext = os.path.splitext(_filename)
+            new_filename = _file + file_insert + _ext
+            new_val = os.path.join(_path, insert, new_filename)
+            type2filepattern[key] = new_val
+        y_axis = (-4, 4)
+
+    else:
+        y_axis = (-0.4, 0.8) # set empirically based on average gaps
+
     num_models = len(type2filepattern)
 
 
     master_df = pd.DataFrame()
-    #y_axis = (-0.2, 0.4) # min and max of the output
-    y_axis = (-0.4, 0.8)
-
-    #convergence_dict = compressed2convergence if args.compressed else lang2convergence
 
     for model_type, file_pattern in type2filepattern.items():
             print(model_type)
@@ -87,19 +102,28 @@ if __name__ == "__main__":
     mask = master_df["bias_type"] == "rank"
     master_df = master_df[~mask]
 
-    # stat sig
-    significance_mask = master_df["statistical_significance"] > (0.05/num_models)
-    sig_df = master_df[significance_mask]
-    myplot = sns.stripplot(data=master_df, x="model_type", y="performance_gap", hue="bias_type", order=type_order)
-    myplot = sns.stripplot(data=sig_df, x="model_type", y="performance_gap", color="black", s=100, order=type_order, marker="x")
-    #myplot.set(ylabel=None, yticklabels=[])
-    #myplot.tick_params(left=False)
-    myplot.axhline(0.0, linestyle=":", color="gray")
-    plt.xticks(rotation=90)
-    plt.ylim(*y_axis)
-    plt.savefig(os.path.join(args.output_dir, f"all_models_{args.lang}.pdf"))
-    #plt.clf()
-    
-
-
+    # break out by bias_type
+    lang = args.lang if args.lang != "en_scrubbed" else "en"
+    bias_types = lang2bias[lang]
+    for bt in bias_types.keys():
+        if bt == "gender":
+            colour = "mediumblue"
+        elif bt == "race":
+            colour = "firebrick"
+        mask = master_df["bias_type"] == bt
+        this_df = master_df[mask]
+        myplot = sns.stripplot(data=this_df, x="model_type", y="performance_gap", color=colour, order=type_order, jitter=True, dodge=True)
+        if args.plot_type == "scatter":
+            # stat sig
+            significance_mask = this_df["statistical_significance"] > (0.05/num_models)
+            sig_df = this_df[significance_mask]
+            myplot = sns.stripplot(data=sig_df, x="model_type", y="performance_gap", color="black", s=100, order=type_order, marker="x")
+        #myplot.set(ylabel=None, yticklabels=[])
+        #myplot.tick_params(left=False)
+        myplot.axhline(0.0, linestyle=":", color="gray")
+        plt.xticks(rotation=90)
+        plt.ylim(*y_axis)
+        plt.subplots_adjust(bottom=0.28)
+        plt.savefig(os.path.join(args.output_dir, f"all_models_{args.lang}_{bt}.pdf"))
+        plt.clf()
             
