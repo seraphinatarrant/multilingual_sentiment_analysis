105143122e37f7e13634fc5534d64dae71e45a92
seraphinatarrant <serif@uw.edu>
2022-05-12 12:18:15 +0100

    add heatmap confusion_matrix

--- plot_all_by_language.py
+++ plot_all_by_language.py
@@ -1,14 +1,16 @@
 import argparse
 import os
 import sys
+#import ipdb
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from plotnine import ggplot, aes, geom_count
+from sklearn import metrics
 
 from utils.model_utils import lang2convergence, compressed2convergence, multi_convergence, \
-    compressed_multi_convergence, balanced_multi_convergence, compressed_balanced_multi_convergence
+    compressed_multi_convergence, balanced_multi_convergence, compressed_balanced_multi_convergence, mono_multi2convergence
 
 from evaluation.create_eval_set import lang2bias
 
@@ -30,6 +32,8 @@ def get_convergence_by_type(model_type, lang):
         steps = balanced_multi_convergence
     elif model_type == "multi_xl_c_b":
         steps = compressed_balanced_multi_convergence
+    elif model_type == "multi_on_mono":
+        steps = mono_multi2convergence[lang]
     else:
         sys.exit("Not a valid model type to check for convergence: {}".format(model_type))
 
@@ -40,8 +44,8 @@ def setup_argparse():
     p = argparse.ArgumentParser()
     p.add_argument('-l', '--lang', dest='lang')
     p.add_argument('-o', dest='output_dir', default='analysis/plot_all/', help='output dir')
-    p.add_argument('-pt', '--plot_type', choices=['swarm', 'scatter', 'bubble', 'violin', "strip"],
-                   default="swarm")
+    p.add_argument('-pt', '--plot_type', choices=['heatmap', 'scatter', 'bubble', 'violin', "errbars"],
+                   default="errbars")
 
     return p.parse_args()
 
@@ -51,12 +55,13 @@ if __name__ == "__main__":
 
     print(args)
 
-    type_order = ["baseline", "mono", "mono_c", "multi_xl",
+    type_order = ["baseline", "mono", "multi_on_mono", "mono_c", "multi_xl",
                   "multi_xl_c", "multi_xl_b", "multi_xl_c_b"]
 
     type2filepattern = {
         "baseline": "results/baseline/{}/{}_ensemble.csv",
         "mono": "results/{}/{}_ensemble.csv",
+        "multi_on_mono": "results/mono_multi/{}/multi+{}_{}_ensemble.csv",
         "multi_xl": "results/{}/multi+en_{}_ensemble.csv",
         "mono_c": "results/compressed/{}/{}_ensemble.csv",
         "multi_xl_c": "results/compressed/{}/multi+en_{}_ensemble.csv",
@@ -73,20 +78,21 @@ if __name__ == "__main__":
             new_filename = _file + file_insert + _ext
             new_val = os.path.join(_path, insert, new_filename)
             type2filepattern[key] = new_val
-        y_axis = (-4, 4)
+    if args.plot_type == "scatter" or args.plot_type == "errbars":
+        y_axis = (-0.4, 0.8) # set empirically based on average gaps  
 
     else:
-        y_axis = (-0.4, 0.8) # set empirically based on average gaps
+        y_axis = (-4, 4)
 
     num_models = len(type2filepattern)
 
-
+    # This all just makes the correct dataframes
     master_df = pd.DataFrame()
 
     for model_type, file_pattern in type2filepattern.items():
             print(model_type)
             try:
-                infile = file_pattern.format(args.lang, args.lang)
+                infile = file_pattern.format(args.lang, args.lang) if model_type != "mono_multi" else file_pattern.format(args.lang, args.lang, args.lang)
                 df = pd.read_csv(infile)
             except:
                 print("Couldn't read in file, may not exist: {}".format(infile), file=sys.stderr)
@@ -98,12 +104,12 @@ if __name__ == "__main__":
                 mask = df["steps"] == convergence_steps
                 df = df[mask]
             df["model_type"] = model_type
-            master_df = master_df.append(df)
-            #myplot = sns.scatterplot(data=df, x="steps", y="performance_gap")
+            master_df = master_df.append(df, ignore_index=True)
 
-    mask = master_df["bias_type"] == "rank"
+    mask = master_df["bias_type"] == "rank" # artifact of a tested bias type in Japanese that no longer use
     master_df = master_df[~mask]
 
+    ## This is where the plotting happens
     # break out by bias_type
     lang = args.lang if args.lang != "en_scrubbed" else "en"
     bias_types = lang2bias[lang]
@@ -117,33 +123,59 @@ if __name__ == "__main__":
         elif bt == "race":
             colour = "firebrick"
 
-        # set output filename
-        outfile = os.path.join(args.output_dir, f"all_models_{args.lang}_{bt}.pdf")
+        # if doing a confusion matrix heatmap, then need to also do a separate graph for each model
+        if args.plot_type == "heatmap":
+            labels = list(map(str, range(1, 6)))
+            for model_type in type_order:
+                mask = this_df["model_type"] == model_type
+                model_df = this_df[mask]
+                # make confusion matrix and also a zero'd along the diagonal confusion matrix for the agreements (so colours easier to see)
+                cm = metrics.confusion_matrix(df["label_1"].values, df["label_2"].values,
+                                              labels=labels)
+                cm_mod = cm.copy()
+                for i in range(5):
+                    cm_mod[i][i] = 0
+                myplot = sns.heatmap(cm, xticklabels=labels, yticklabels=labels)
+                outfile = os.path.join(args.output_dir, f"{args.lang}_{bt}_{model_type}.pdf")
+                plt.savefig(outfile)
 
-        if args.plot_type == "bubble":
-            myplot = ggplot(this_df, aes(x="model_type", y="performance_gap")) + geom_count(color=colour)
-            myplot.save(outfile)
+                myplot = sns.heatmap(cm, xticklabels=labels, yticklabels=labels)
+                outfile_mod = outfile = os.path.join(args.output_dir,
+                                                     f"{args.lang}_{bt}_{model_type}_zeroes.pdf")
+                plt.savefig(outfile_mod)
+
         else:
-            if args.plot_type == "violin":
-                myplot = sns.violinplot(data=this_df, x="model_type", y="performance_gap", color=colour, order=type_order)
-            elif args.plot_type == 'strip':
-                myplot = sns.stripplot(data=this_df, x="model_type", y="performance_gap",
-                                       color=colour, order=type_order, linestyle='',
-                                       err_style='bars')
+            # set output filename
+            outfile = os.path.join(args.output_dir, f"all_models_{args.lang}_{bt}.pdf")
+            if args.plot_type == "bubble":
+                myplot = ggplot(this_df, aes(x="model_type", y="performance_gap")) + geom_count(color=colour)
+                myplot.save(outfile)
             else:
-                myplot = sns.stripplot(data=this_df, x="model_type", y="performance_gap", color=colour, order=type_order, jitter=True, dodge=True)
-                if args.plot_type == "scatter":
-                    # stat sig
-                    significance_mask = this_df["statistical_significance"] > (0.05/num_models)
-                    sig_df = this_df[significance_mask]
-                    myplot = sns.stripplot(data=sig_df, x="model_type", y="performance_gap", color="black", s=100, order=type_order, marker="x")
-            myplot.set(ylabel=None)#, yticklabels=[])
-            myplot.set(xlabel=None)
-            #myplot.tick_params(left=False)
-            myplot.axhline(0.0, linestyle=":", color="gray")
-            plt.xticks(rotation=90)
-            plt.ylim(*y_axis)
-            plt.subplots_adjust(bottom=0.28)
-            plt.savefig(outfile)
-            plt.clf()
-            
+                if args.plot_type == "violin":
+                    myplot = sns.violinplot(data=this_df, x="model_type", y="performance_gap", color=colour, order=type_order)
+                elif args.plot_type == 'errbars':
+                    #ipdb.set_trace()
+                    myplot = sns.lineplot(data=this_df, x="model_type", y="performance_gap",
+                                           color=colour, linestyle='',
+                                           err_style='bars', marker='o')
+                else:
+                    myplot = sns.stripplot(data=this_df, x="model_type", y="performance_gap", color=colour, order=type_order, jitter=True, dodge=True)
+                    if args.plot_type == "scatter":
+                        # stat sig
+                        significance_mask = this_df["statistical_significance"] > (0.05/num_models)
+                        sig_df = this_df[significance_mask]
+                        myplot = sns.stripplot(data=sig_df, x="model_type", y="performance_gap", color="black", s=100, order=type_order, marker="x")
+                myplot.set(ylabel=None)#, yticklabels=[])
+                myplot.set(xlabel=None)
+                #myplot.tick_params(left=False)
+                if args.plot_type == "errbars" or args.plot_type == "scatter":
+                    myplot.axhspan(0.1,-0.1,alpha=0.2)
+                    #myplot.axhline(0.12, linestyle="--", color="gray")
+                    #myplot.axhline(-0.12, linestyle="--", color="gray")
+                myplot.axhline(0.0, linestyle=":", color="gray")
+                plt.xticks(rotation=90)
+                plt.ylim(*y_axis)
+                plt.subplots_adjust(bottom=0.28)
+                plt.savefig(outfile)
+                plt.clf()
+
